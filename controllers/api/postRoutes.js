8d92871129697677ee4210221274f162a1077e03
Deorren Erive <erive92d@gmail.com>
2023-02-03 08:26:01 -0800

    added post requests

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,15 +1,16 @@
 const router = require('express').Router();
-const { Project } = require('../../models');
+const { Posts } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 router.post('/', withAuth, async (req, res) => {
+  console.log(req.body)
   try {
-    const newProject = await Project.create({
+    const newPost = await Posts.create({
       ...req.body,
       user_id: req.session.user_id,
     });
 
-    res.status(200).json(newProject);
+    res.status(200).json(newPost);
   } catch (err) {
     res.status(400).json(err);
   }
