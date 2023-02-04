358bafcf463d4b0271ff25b8dd590bf066d90cd2
Deorren Erive <erive92d@gmail.com>
2023-02-03 20:10:34 -0800

    minor update

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,47 +1,58 @@
 const newFormHandler = async (event) => {
   event.preventDefault();
 
-  const title = document.querySelector('#post-title').value.trim();
-  const contents = document.querySelector('#post-content').value.trim();
+  const title = document.querySelector("#post-title").value.trim();
+  const contents = document.querySelector("#post-content").value.trim();
 
   if (title && contents) {
     const response = await fetch(`/api/posts`, {
-      method: 'POST',
-      body: JSON.stringify({ title, contents}),
+      method: "POST",
+      body: JSON.stringify({ title, contents }),
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
     });
 
-
     if (response.ok) {
-      document.location.replace('/profile');
+      document.location.replace("/profile");
     } else {
-      alert('Failed to create post');
+      alert("Failed to create post");
     }
   }
 };
 
 const delButtonHandler = async (event) => {
-  if (event.target.hasAttribute('data-id')) {
-    const id = event.target.getAttribute('data-id');
+  if (event.target.hasAttribute("data-id")) {
+    const id = event.target.getAttribute("data-id");
 
     const response = await fetch(`/api/posts/${id}`, {
-      method: 'DELETE',
+      method: "DELETE",
     });
 
     if (response.ok) {
-      document.location.replace('/profile');
+      document.location.replace("/profile");
     } else {
-      alert('Failed to delete post');
+      alert("Failed to delete post");
     }
   }
 };
 
-document
-  .querySelector('.new-post-form')
-  .addEventListener('submit', newFormHandler);
+
+// const commentHandler = async (e) => {
+//   e.preventDefault()
+//   const commentText = document.getElementById("comment-text").value.trim()
+//   const response = await fetch(`/api/posts/`)
+
+// }
 
 document
-  .querySelector('.post-list')
-  .addEventListener('click', delButtonHandler);
+  .querySelector(".new-post-form")
+  .addEventListener("submit", newFormHandler);
+
+document
+  .querySelector(".post-list")
+  .addEventListener("click", delButtonHandler);
+
+// document
+//   .getElementById("comment-form")
+//   .addEventListener("submit", commentHandler);
