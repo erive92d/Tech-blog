071811f732bc3607e266069b7163d2efa1bdf5b4
Deorren Erive <erive92d@gmail.com>
2023-02-04 21:19:00 -0800

    testing heroku

--- config/connection.js
+++ config/connection.js
@@ -6,11 +6,14 @@ const sequelize = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    host: '127.0.0.1',
+    host: 'localhost',
     dialect: 'mysql',
     port: 3306,
     dialectOptions: {
-      ssl: true
+      ssl: {
+        require:true,
+        rejectUnauthorized:false
+      }
     }
   }
 );
