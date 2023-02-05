305720fe881ba3a24d1c68501a061c532f72f042
Deorren Erive <erive92d@gmail.com>
2023-02-04 20:52:45 -0800

    testing

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -29,7 +29,7 @@ router.delete('/:id', withAuth, async (req, res) => {
     });
 
     if (!postData) {
-      res.status(404).json({ message: 'No project found with this id!' });
+      res.status(404).json({ message: 'No Post found with this id!' });
       return;
     }
 
